258a3e6bde6936064a7962e89340284771474fce
ZeppelinGames <zeppelingames3@gmail.com>
2021-04-25 00:38:28 +1000

    Doors and keys work

---
+++ Assets/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private List<int> inventoryItems = new List<int>();
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void AddItem(int id)
+    {
+        inventoryItems.Add(id);
+    }
+    public void RemoveItem(int id)
+    {
+        inventoryItems.Remove(id);
+    }
+
+    public bool hasItem(int id)
+    {
+        return inventoryItems.Contains(id);
+    }
+}
--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -6,13 +6,23 @@ using UnityEngine.Events;
 public class Trigger : MonoBehaviour
 {
     public UnityEvent events;
-    public LayerMask triggerLayers;
+    public List<int> itemCheck = new List<int>();
+     public LayerMask triggerLayers;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (triggerLayers == (triggerLayers | (1 << collision.gameObject.layer)))
         {
-            events.Invoke();
+            bool hasItems = true;
+            foreach (int itemId in itemCheck) {
+                if (!Inventory.instance.hasItem(itemId)) {
+                    hasItems = false;
+                }
+            }
+
+            if (hasItems) {
+                events.Invoke();
+            }
         }
     }
 }
